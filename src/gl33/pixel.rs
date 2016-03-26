0e33ea817b67f0c877ca3267ed42dd0b02306010
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-03-26 21:55:41 +0100

    Added OpenGL type in pixel recognition function.

--- src/gl33/pixel.rs
+++ src/gl33/pixel.rs
@@ -2,14 +2,14 @@ use gl;
 use gl::types::*;
 use luminance::pixel::{Format, PixelFormat, Type};
 
-// Return the format and internal sized-format.
-pub fn gl_pixel_format(pf: &PixelFormat) -> Option<(GLenum, GLenum)> {
+// Return the format, internal sized-format and type.
+pub fn gl_pixel_format(pf: &PixelFormat) -> Option<(GLenum, GLenum, GLenum)> {
   match (pf.format, pf.encoding) {
-    (Format::RGB(8, 8, 8), Type::Unsigned) => Some((gl::RGB_INTEGER, gl::RGB8UI)),
-    (Format::RGBA(8, 8, 8, 8), Type::Unsigned) => Some((gl::RGBA_INTEGER, gl::RGBA8UI)),
-    (Format::RGB(32, 32, 32), Type::Floating) => Some((gl::RGB, gl::RGB32F)),
-    (Format::RGBA(32, 32, 32, 32), Type::Floating) => Some((gl::RGBA, gl::RGBA32F)),
-    (Format::Depth(32), Type::Floating) => Some((gl::DEPTH_COMPONENT, gl::DEPTH_COMPONENT32F)),
+    (Format::RGB(8, 8, 8), Type::Unsigned) => Some((gl::RGB_INTEGER, gl::RGB8UI, gl::UNSIGNED_BYTE)),
+    (Format::RGBA(8, 8, 8, 8), Type::Unsigned) => Some((gl::RGBA_INTEGER, gl::RGBA8UI, gl::UNSIGNED_BYTE)),
+    (Format::RGB(32, 32, 32), Type::Floating) => Some((gl::RGB, gl::RGB32F, gl::FLOAT)),
+    (Format::RGBA(32, 32, 32, 32), Type::Floating) => Some((gl::RGBA, gl::RGBA32F, gl::FLOAT)),
+    (Format::Depth(32), Type::Floating) => Some((gl::DEPTH_COMPONENT, gl::DEPTH_COMPONENT32F, gl::FLOAT)),
     _ => None
   }
 }
