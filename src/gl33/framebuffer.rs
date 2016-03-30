56c143ea732baadd70ded628e27a49a9a8d8661c
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-30 18:39:32 +0200

    YAY.

--- src/gl33/framebuffer.rs
+++ src/gl33/framebuffer.rs
@@ -21,33 +21,35 @@ impl HasFramebuffer for GL33 {
           CS: ColorSlot<Self, L, D>,
           DS: DepthSlot<Self, L, D> {
     let mut framebuffer: GLuint = 0;
-    let mut textures: GLuint = 0;
     let color_formats = CS::color_formats();
     let depth_format = DS::depth_format();
 
     unsafe {
       let target = to_target(L::layering(), D::dim());
+      let mut textures: Vec<Self::ATexture> = Vec::with_capacity(color_formats.len() + if depth_format.is_some() { 1 } else { 0 }); // FIXME: remove that (inference)
 
       gl::GenFramebuffers(1, &mut framebuffer);
 
       gl::BindFramebuffer(gl::FRAMEBUFFER, framebuffer);
 
       // generate all the required textures with the correct formats
-      gl::GenTextures((color_formats.len() + if depth_format.is_some() { 1 } else { 0 }) as GLint, &mut textures);
+      gl::GenTextures((textures.len()) as GLint, textures.as_mut_ptr());
 
       // color textures
       if color_formats.is_empty() {
         gl::DrawBuffer(gl::NONE);
       } else {
-        for (i, format) in color_formats.iter().enumerate() {
-          gl::BindTexture(target, textures + i as GLuint);
+        for (format, texture) in color_formats.iter().zip(&textures) {
+          gl::BindTexture(target, *texture);
           create_texture::<L, D>(target, size, mipmaps, *format, &Default::default());
         }
       }
 
       // depth texture, if exists
       if let Some(format) = depth_format {
-        gl::BindTexture(target, textures + color_formats.len() as GLuint);
+        let depth_texture = textures.pop().unwrap();
+
+        gl::BindTexture(target, depth_texture);
         create_texture::<L, D>(target, size, mipmaps, format, &Default::default());
       } else {
         // TODO: create render buffer
