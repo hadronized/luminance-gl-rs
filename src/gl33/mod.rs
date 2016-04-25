0492eb91cc454cd0a4d572ca285020ccbba15952
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-04-25 16:23:24 +0200

    Framebuffer and Slot in the public interface.

--- src/gl33/mod.rs
+++ src/gl33/mod.rs
@@ -8,6 +8,7 @@ pub mod texture;
 pub mod token;
 
 pub use self::buffer::Buffer;
+pub use self::framebuffer::{Framebuffer, Slot};
 pub use self::render::{FrameCommand, ShadingCommand, RenderCommand};
 pub use self::shader::program::Program;
 pub use self::shader::stage::Stage;
